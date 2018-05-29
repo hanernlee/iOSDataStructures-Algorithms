e61b6b22e9717324ab1ecda192d6df754d862309
Christopher Lee <hanernlee@gmail.com>
2018-05-29 23:23:58 +1000

    Collection implementation

--- Chapter3-LinkedList/LinkedList.playground/Contents.swift
+++ Chapter3-LinkedList/LinkedList.playground/Contents.swift
@@ -86,5 +86,19 @@ example(of: "removing a node after a particular node") {
     
     print("After removing at index \(index): \(list)")
     print("Removed value:" + String(describing: removedValue))
+}
+
+example(of: "using collection") {
+    var list = LinkedList<Int>()
+    for i in 0...9 {
+        list.append(i)
+    }
     
+    print("List: \(list)")
+    print("First element: \(list[list.startIndex])")
+    print("Array containing first 3 elements: \(Array(list.prefix(3)))")
+    print("Array containing last 3 elements: \(Array(list.suffix(3)))")
+    
+    let sum = list.reduce(0, +)
+    print("Sum of all values: \(sum)")
 }
--- Chapter3-LinkedList/LinkedList.playground/Sources/LinkedList.swift
+++ Chapter3-LinkedList/LinkedList.playground/Sources/LinkedList.swift
@@ -101,3 +101,45 @@ extension LinkedList {
         return String(describing: head)
     }
 }
+
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public var node: Node<Value>?
+        
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+        
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+    
+    public var startIndex: Index {
+        return Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        return Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        return Index(node: i.node?.next)
+    }
+    
+    public subscript(position: Index) -> Value {
+        return position.node!.value
+    }
+}
