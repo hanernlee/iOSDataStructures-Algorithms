7f3877dee34f3465bcb81c9ffd3496fc2381274d
Christopher Lee <hanernlee@gmail.com>
2018-06-09 21:58:20 +1000

    Complete stack

--- Chapter3-LinkedList/LinkedList.playground/Sources/Stack.swift
+++ Chapter3-LinkedList/LinkedList.playground/Sources/Stack.swift
@@ -2,7 +2,27 @@ import Foundation
 
 public struct Stack<Element> {
     private var storage: [Element] = []
-    public init() { }
+    
+    public init(_ elements: [Element]) {
+        storage = elements
+    }
+    
+    public mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    @discardableResult
+    public mutating func pop() -> Element? {
+        return storage.popLast()
+    }
+    
+    public func peek() -> Element? {
+        return storage.last
+    }
+    
+    public var isEmpty: Bool {
+        return peek() == nil
+    }
 }
 
 extension Stack: CustomStringConvertible {
@@ -19,4 +39,9 @@ extension Stack: CustomStringConvertible {
     }
 }
 
+extension Stack: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
+}
 
