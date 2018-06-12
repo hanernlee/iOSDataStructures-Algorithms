17269f93da1429b7a956b76e689f642ff4e5b28c
Christopher Lee <hanernlee@gmail.com>
2018-06-12 21:29:19 +1000

    QueueLinkedList

---
+++ Chapter5-Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+var queue = QueueLinkedList<String>()
+queue.enqueue("Ray")
+queue.enqueue("Brian")
+queue.enqueue("Eric")
+queue.dequeue()
+queue
+queue.peek
--- Chapter5-Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Sources/DoublyLinkedList.swift
+++ Chapter5-Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Sources/DoublyLinkedList.swift
@@ -104,3 +104,36 @@ extension DoublyLinkedList: Sequence {
     return LinkedListIterator(node: head)
   }
 }
+
+public class QueueLinkedList<T>: Queue {
+    private var list = DoublyLinkedList<T>()
+    public init() {}
+    
+    public func enqueue(_ element: T) -> Bool {
+        list.append(element)
+        return true
+    }
+    
+    public func dequeue() -> T? {
+        guard !list.isEmpty, let element = list.first else {
+            return nil
+        }
+        
+        return list.remove(element)
+    }
+    
+    public var peek: T? {
+        return list.first?.value
+    }
+    
+    public var isEmpty: Bool {
+        return list.isEmpty
+    }
+}
+
+extension QueueLinkedList: CustomStringConvertible {
+    public var description: String {
+        return list.description
+    }
+}
+
