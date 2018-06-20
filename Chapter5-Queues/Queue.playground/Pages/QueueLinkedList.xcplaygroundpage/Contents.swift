951a5c3bd420842c49938cbbf8f3040dd36f7afd
Christopher Lee <hanernlee@gmail.com>
2018-06-20 21:49:25 +1000

    Queue Stacks

--- Chapter5-Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Contents.swift
+++ Chapter5-Queues/Queue.playground/Pages/QueueLinkedList.xcplaygroundpage/Contents.swift
@@ -1,3 +1,35 @@
+public class QueueLinkedList<T>: Queue {
+    private var list = DoublyLinkedList<T>()
+    public init() {}
+    
+    public func enqueue(_ element: T) -> Bool {
+        list.append(element)
+        return true
+    }
+    
+    public func dequeue() -> T? {
+        guard !list.isEmpty, let element = list.first else {
+            return nil
+        }
+        
+        return list.remove(element)
+    }
+    
+    public var peek: T? {
+        return list.first?.value
+    }
+    
+    public var isEmpty: Bool {
+        return list.isEmpty
+    }
+}
+
+extension QueueLinkedList: CustomStringConvertible {
+    public var description: String {
+        return list.description
+    }
+}
+
 var queue = QueueLinkedList<String>()
 queue.enqueue("Ray")
 queue.enqueue("Brian")
--- Chapter5-Queues/Queue.playground/Pages/QueueStack.xcplaygroundpage/Contents.swift
+++ Chapter5-Queues/Queue.playground/Pages/QueueStack.xcplaygroundpage/Contents.swift
@@ -1,3 +1,8 @@
 // Copyright (c) 2018 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+public struct QueueStack<T>: Queue {
+    private var leftStack: [T] = []
+    private var rightStack: [T] = []
+    public init {}
+}
