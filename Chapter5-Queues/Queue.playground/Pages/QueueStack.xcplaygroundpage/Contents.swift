1b06fae2735d650d050621577760f8e789b1775e
Christopher Lee <hanernlee@gmail.com>
2018-06-20 21:59:08 +1000

    Finish up Queues

--- Chapter5-Queues/Queue.playground/Pages/QueueStack.xcplaygroundpage/Contents.swift
+++ Chapter5-Queues/Queue.playground/Pages/QueueStack.xcplaygroundpage/Contents.swift
@@ -5,4 +5,41 @@ public struct QueueStack<T>: Queue {
     private var leftStack: [T] = []
     private var rightStack: [T] = []
     public init {}
+    
+    public var isEmpty: Bool {
+        return leftStack.isEmpty && rightStack.isEmpty
+    }
+    
+    public var peek: T? {
+        return !leftStack.isEmpty ? leftStack.last : rightStack.first
+    }
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        rightStack.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        if leftStack.isEmpty {
+            leftStack = rightStack.reversed()
+            rightStack.removeAll()
+        }
+        
+        return leftStack.popLast()
+    }
 }
+
+extension QueueStack: CustomStringConvertible {
+    public var description: String {
+        let printList = leftStack + rightStack.reversed()
+        return printList.description
+    }
+}
+
+var queue = QueueStack<String>()
+queue.enqueue("Ray")
+queue.enqueue("Brian")
+queue.enqueue("Eric")
+queue.dequeue()
+queue
+queue.peek
