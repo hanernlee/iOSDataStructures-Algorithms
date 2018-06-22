7605fe5486e20398528978db36320147ca88122c
Christopher Lee <hanernlee@gmail.com>
2018-06-22 22:42:22 +1000

    TreeNode

--- Chapter6-Trees/GeneralPurposeTree.playground/Contents.swift
+++ Chapter6-Trees/GeneralPurposeTree.playground/Contents.swift
@@ -1,3 +1,12 @@
 // Copyright (c) 2018 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+example(of: "creating a tree") {
+    let beverages = TreeNode("Beverages")
+    
+    let hot = TreeNode("Hot")
+    let cold = TreeNode("Cold")
+    
+    beverages.add(hot)
+    beverages.add(cold)
+}
