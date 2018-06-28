a026a2914194a2733271a055fe2ba567fca9d457
Christopher Lee <hanernlee@gmail.com>
2018-06-29 00:22:30 +1000

    Search for node

--- Chapter6-Trees/GeneralPurposeTree.playground/Contents.swift
+++ Chapter6-Trees/GeneralPurposeTree.playground/Contents.swift
@@ -61,3 +61,15 @@ example(of: "level-order traversal") {
     tree.forEachLevelOrder { print($0.value) }
 }
 
+example(of: "searching for a node") {
+    if let searchResult1 = tree.search("ginger ale") {
+        print("Found node: \(searchResult1.value)")
+    }
+    
+    if let searchResult2 = tree.search("WKD Blue") {
+        print(searchResult2)
+    } else {
+        print("Couldn't find WKD Blue")
+    }
+}
+
