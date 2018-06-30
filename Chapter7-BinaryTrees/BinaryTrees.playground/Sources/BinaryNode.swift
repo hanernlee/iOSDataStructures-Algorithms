362b9656b42aed54601b27990ba4f737b1840a41
Christopher Lee <hanernlee@gmail.com>
2018-06-30 19:37:14 +1000

    Build tree node

---
+++ Chapter7-BinaryTrees/BinaryTrees.playground/Sources/BinaryNode.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public class BinaryNode<Element> {
+    public var value: Element
+    public var leftChild: BinaryNode?
+    public var rightChild: BinaryNode?
+    
+    public init(value: Element) {
+        self.value = value
+    }
+}
