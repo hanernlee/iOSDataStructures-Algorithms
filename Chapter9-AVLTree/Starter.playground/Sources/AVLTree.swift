92369051993b4190e3525a1020c16816d2aeabd3
Christopher Lee <hanernlee@gmail.com>
2018-07-03 23:27:23 +1000

    AVL

---
+++ Chapter9-AVLTree/Starter.playground/Sources/AVLTree.swift
@@ -0,0 +1,103 @@
+// Copyright (c) 2018 Razeware LLC
+// For full license & permission details, see LICENSE.markdown.
+
+public struct AVLTree<Element: Comparable> {
+  
+  public private(set) var root: AVLNode<Element>?
+  
+  public init() {}
+}
+
+extension AVLTree: CustomStringConvertible {
+  
+  public var description: String {
+    return root?.description ?? "empty tree"
+  }
+}
+
+extension AVLTree {
+  
+  public mutating func insert(_ value: Element) {
+    root = insert(from: root, value: value)
+  }
+  
+  private func insert(from node: AVLNode<Element>?, value: Element) -> AVLNode<Element> {
+    guard let node = node else {
+      return AVLNode(value: value)
+    }
+    if value < node.value {
+      node.leftChild = insert(from: node.leftChild, value: value)
+    } else {
+      node.rightChild = insert(from: node.rightChild, value: value)
+    }
+    return node
+  }
+
+    private func leftRotate(_ node: AVLNode<Element>) -> AVLNode<Element> {
+        let pivot = node.rightChild!
+        
+        node.rightChild = pivot.leftChild
+        pivot.leftChild = node
+        
+        node.height = max(node.leftHeight, node.rightHeight) + 1
+        pivot.height = max(pivot.leftHeight, pivot.rightHeight) + 1
+        
+        return pivot
+    }
+}
+
+extension AVLTree {
+  
+  public func contains(_ value: Element) -> Bool {
+    var current = root
+    while let node = current {
+      if node.value == value {
+        return true
+      }
+      if value < node.value {
+        current = node.leftChild
+      } else {
+        current = node.rightChild
+      }
+    }
+    return false
+  }
+}
+
+private extension AVLNode {
+  
+  var min: AVLNode {
+    return leftChild?.min ?? self
+  }
+}
+
+extension AVLTree {
+  
+  public mutating func remove(_ value: Element) {
+    root = remove(node: root, value: value)
+  }
+  
+  private func remove(node: AVLNode<Element>?, value: Element) -> AVLNode<Element>? {
+    guard let node = node else {
+      return nil
+    }
+    if value == node.value {
+      if node.leftChild == nil && node.rightChild == nil {
+        return nil
+      }
+      if node.leftChild == nil {
+        return node.rightChild
+      }
+      if node.rightChild == nil {
+        return node.leftChild
+      }
+      node.value = node.rightChild!.min.value
+      node.rightChild = remove(node: node.rightChild, value: node.value)
+    } else if value < node.value {
+      node.leftChild = remove(node: node.leftChild, value: value)
+    } else {
+      node.rightChild = remove(node: node.rightChild, value: value)
+    }
+    return node
+  }
+}
