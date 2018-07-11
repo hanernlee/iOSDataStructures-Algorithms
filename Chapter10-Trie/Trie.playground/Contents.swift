b1dbb22768e36063b10b60c93a11413de235b32a
Christopher Lee <hanernlee@gmail.com>
2018-07-11 23:35:36 +1000

    Trie methods

--- Chapter10-Trie/Trie.playground/Contents.swift
+++ Chapter10-Trie/Trie.playground/Contents.swift
@@ -1,3 +1,10 @@
 // Copyright (c) 2018 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+example(of: "insert and contain") {
+    let trie = Trie<String>
+    trie.insert("cute")
+    if trie.contains("cute") {
+        print("cute is in the trie")
+    }
+}
--- Chapter10-Trie/Trie.playground/Sources/Trie.swift
+++ Chapter10-Trie/Trie.playground/Sources/Trie.swift
@@ -1,7 +1,44 @@
 import Foundation
 
-public class Trie<CollectinoType: Collection where CollectionType.Element: Hashable> {
+public class Trie<CollectionType: Collection> where CollectionType.Element: Hashable {
     public typealias Node = TrieNode<CollectionType.Element>
     private let root = Node(key: nil, parent: nil)
     public init() {}
+    
+    public func insert(_ collection: CollectionType) {
+        var current = root
+        
+        for element in collection {
+            if current.children[element] == nil {
+                current.children[element] = Node(key: element, parent: current)
+            }
+            current = current.children[element]!
+        }
+        current.isTerminating = true
+    }
+    
+    public func contains(_ collection: CollectionType) -> Bool {
+        var current = root
+        for element in collection {
+            guard let child = current.children[element] else { return false }
+            current = child
+        }
+        return current.isTerminating
+    }
+    
+    public func remove(_ collection: CollectionType) {
+        var current = root
+        for element in collection {
+            guard let child = current.children[element] else {
+                return
+            }
+            current = child
+        }
+        guard current.isTerminating else { return }
+        current.isTerminating = false
+        while let parent = current.parent, current.children.isEmpty && !current.isTerminating {
+            parent.children[current.key!] = nil
+            current = parent
+        }
+    }
 }
